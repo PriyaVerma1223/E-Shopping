82aa4264d82e85bbd2c5420695f606b60bcf6290
Priya Verma <priya097verma@gmail.com>
2024-08-13 14:24:47 +0530

    Add files via upload

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -4,8 +4,9 @@ import Category from '../../components/category/Category'
 import HomePageProductCard from '../../components/homePageProductCard/HomePageProductCard'
 import Track from '../../components/track/Track'
 import Testimonial from '../../components/testimonial/Testimonial'
-import Loader from '../../components/loader/Loader'
-import UserDetail from '../../components/admin/UserDetail'
+// import Loader from '../../components/loader/Loader'
+// import UserDetail from '../../components/admin/UserDetail'
+import { Link } from 'react-router-dom'
 
 const  HomePage = () => {
   
@@ -14,9 +15,14 @@ const  HomePage = () => {
         <HeroSection />
         <Category />
         <HomePageProductCard />
+        <div className="flex justify-center -mt-1 mb-3 ">
+        <Link to={'/allproduct'}>
+          <button className='text-white mt-10 px-8 py-2 rounded-xl bg-indigo-600 hover:bg-indigo-700'>See more</button>
+        </Link>
+      </div>
         <Track />
         <Testimonial />
-        <Loader />
+        {/* <Loader /> */}
         
     </Layout>
   )
